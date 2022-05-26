c3156ef11a09834fb342fb6a1c66a32be8e40da6
Payhaev17 <ran777dom@gmail.com>
2022-05-26 18:55:51 +0300

    changes

--- back/index.ts
+++ back/index.ts
@@ -1,16 +1,28 @@
 import { Application } from "express";
+import { Connection } from "mysql2/promise";
 import usersRouter from "./routes/Users";
 
+require("dotenv").config({ path: ".env." + process.env.NODE_ENV });
 const express = require("express");
+const mysql = require("mysql2/promise");
+const isDevelopment = process.env.NODE_ENV === "development";
 
-const app: Application = express();
-const PORT = process.env.PORT || 1000;
+async function startApplication() {
+  const app: Application = express();
+  const PORT = process.env.PORT || 1000;
 
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ limit: "10kb" }));
+  const mysqlConnection: Connection = await mysql.createConnection({
+    host: "localhost",
+  });
 
-app.use("/", usersRouter);
+  app.use(express.json({ limit: "10kb" }));
+  app.use(express.urlencoded({ limit: "10kb" }));
 
-app.listen(PORT, () => {
-  console.log("Server listen on " + PORT + "...");
-});
+  app.use("/", usersRouter);
+
+  app.listen(PORT, () => {
+    console.log("Server listen on " + PORT + "...");
+  });
+}
+
+startApplication();
